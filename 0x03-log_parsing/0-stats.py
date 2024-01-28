1daed70853d3052f7b7a54a86eed80e2a2174509
Bakr Ibrahimi <bakribra5@gmail.com>
2024-01-28 21:26:14 +0100

    add all task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,43 @@
 #!/usr/bin/python3
+"""
+Python script takes URL from stdin and compute exact metrics
+"""
 
-""" reads stdin line by line and computes metric"""
 
-if __name__ == '__main__':
+def printst(dictionary, size):
+    """
+    Print function
+    """
+    d = sorted(dictionary.keys())
+    print("File size: {:d}".format(size))
+    for i in d:
+        if dictionary[i] != 0:
+            print("{}: {:d}".format(i, dictionary[i]))
 
-    def printer(file_size, status):
+count = 0
+size = 0
+dic = {"200": 0, "301": 0, "400": 0, "401": 0,
+       "403": 0, "404": 0, "405": 0, "500": 0}
 
-        '''Print logs'''
+try:
+    with open(0) as f:
+        for line in f:
+            count += 1
+            arr = line.split()
+            try:
+                size += int(arr[-1])
+            except:
+                pass
+            try:
+                st = arr[-2]
+                if st in dic:
+                    dic[st] += 1
+            except:
+                pass
+            if count % 10 == 0:
+                printst(dic, size)
+        printst(dic, size)
 
-        print("File size: {:d}".format(file_size))
-        for i in sorted(status.keys()):
-            if status[i] != 0:
-                print("{}: {}".format(i, status[i]))
-
-    file_size = 0
-    status = {"200": 0, "301": 0, "400": 0, "401": 0,
-              "403": 0, "404": 0, "405": 0, "500": 0}
-
-    counter = 0
-    try:
-        with open(0) as f:
-            for line in f:
-                counter += 1
-                data = line.split()
-
-                try:
-                    file_size += int(data[-1])
-                except Exception:
-                    pass
-
-                try:
-                    st = data[-2]
-                    if st in status:
-                        status[st] += 1
-
-                except Exception:
-                    pass
-                if counter % 10 == 0:
-                    printer(file_size, status)
-            printer(file_size, status)
-    except KeyboardInterrupt:
-        printer(file_size, status)
-        raise
+except KeyboardInterrupt:
+    printst(dic, size)
+    raise
